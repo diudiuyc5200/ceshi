1e211e9c501a8e75546ce41f895297e4a98c597f
diudiuyc5200 <17766728994@163.com>
2026-07-01 17:12:18 +0800

    补全lz4缺失文件

--- include/linux/lz4.h
+++ include/linux/lz4.h
@@ -9,6 +9,17 @@
 
 #define LZ4_MEM_COMPRESS	LZ4_STREAM_MINSIZE
 #define LZ4HC_MEM_COMPRESS	LZ4_STREAMHC_MINSIZE
+/*-************************************************************************
+ *	CONSTANTS
+ **************************************************************************/
+/*
+ * LZ4_MEMORY_USAGE :
+ * Memory usage formula : N->2^N Bytes
+ * (examples : 10 -> 1KB; 12 -> 4KB ; 16 -> 64KB; 20 -> 1MB; etc.)
+ * Increasing memory usage improves compression ratio
+ * Reduced memory usage can improve speed, due to cache effect
+ * Default value is 14, for 16KB, which nicely fits into Intel x86 L1 cache
+ */
 
 #define LZ4HC_MIN_CLEVEL	LZ4HC_CLEVEL_MIN
 #define LZ4HC_MAX_CLEVEL	LZ4HC_CLEVEL_MAX
